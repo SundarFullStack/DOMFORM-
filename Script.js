7160eb032a10faa373f2f7fb10840f588e44b8d5
SundarFullStack <126937777+SundarFullStack@users.noreply.github.com>
2023-04-09 12:35:25 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,21 @@
+
+function formData() {
+    
+    let firstname = document.getElementById('firstname').value;
+
+    console.log(`Your Firstname: ${firstname}`);
+
+    let middlename = document.getElementById('middlename').value;
+
+    console.log(`Your Secondname: ${ middlename }`);
+    
+    let lastname = document.getElementById('lastname').value;
+
+    console.log(`Your Thirdname: ${lastname}`);
+
+    let mail = document.getElementById('mailId').value;
+
+    console.log(`Your Mail: ${mail}`);
+    
+
+}
